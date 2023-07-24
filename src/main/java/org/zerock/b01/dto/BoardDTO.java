5bdbbc30baf8e71bb1e5faa3ad0e63ce21a7b49f
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-24 17:30:39 +0900

    컨트롤러와 화면 처리

--- src/main/java/org/zerock/b01/dto/BoardDTO.java
+++ src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -2,6 +2,8 @@ package org.zerock.b01.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,10 +14,14 @@ public class BoardDTO {
 
     private Long bno;
 
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
 
+    @NotEmpty
     private String content;
 
+    @NotEmpty
     private String writer;
 
     private LocalDateTime regDate;
