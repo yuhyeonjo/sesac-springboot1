5d9293bd8386af907692b1ea5bebe3bb066c0da5
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-24 13:03:41 +0900

    스프링 부트 설정

---
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -0,0 +1,18 @@
+package org.zerock.b01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        log.info("hello..............");
+
+        model.addAttribute("msg", "HELLO WORLD");
+    }
+}
---
+++ src/main/java/org/zerock/b01/controller/SampleJSONController.java
@@ -0,0 +1,17 @@
+package org.zerock.b01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // JSON 형태로 데이터 전달
+@Log4j2
+public class SampleJSONController {
+
+    @GetMapping("/helloArr")
+    public String[] helloArr() {
+        log.info("helloArr.................");
+
+        return new String[] {"AAA", "BBB", "CCC"};
+    }
+}
